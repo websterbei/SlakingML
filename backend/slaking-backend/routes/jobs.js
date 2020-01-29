1ca5206e919ff8d28ed645f16a9152f61a2aa3fd
Webster Bei Yijie <yijie.bei@duke.edu>
2020-01-29 15:25:14 -0500

    added job_id filed

--- backend/slaking-backend/routes/jobs.js
+++ backend/slaking-backend/routes/jobs.js
@@ -37,6 +37,7 @@ router.get('/:jobId', function(req, res, next) {
       res.status(500);
       res.send("Failed to retrieve the list of jobs!");
     } else {
+      dbres.job_id = dbres._id;
       res.send(dbres);
     }
   });
