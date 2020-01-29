b7ec9f616604a08a9559133861fde148da7b72b1
Webster Bei Yijie <yijie.bei@duke.edu>
2020-01-29 00:00:25 -0500

    added mongodb to backend

--- backend/slaking-backend/routes/jobs.js
+++ backend/slaking-backend/routes/jobs.js
@@ -1,14 +1,87 @@
 var express = require('express');
 var router = express.Router();
+var ObjectID = require('mongodb').ObjectID;
 
 /* GET jobs listing. */
 router.get('/', function(req, res, next) {
-  res.json({jobs: ["1", "2", "3", "4"]});
+  db = req.app.locals.dbClient.db("jobs");
+  if(req.query.details!="true") {
+    db.collection("jobs").distinct("_id", {}, function(err, dbres) {
+      if (err) {
+        res.status(500);
+        res.send("Failed to retrieve the list of jobs!");
+      } else {
+        res.send({jobs: dbres});
+      }
+    });
+  } else {
+    db.collection("jobs").find({}).toArray(function(err, dbres) {
+      if (err) {
+        res.status(500);
+        res.send("Failed to retrieve the list of jobs!");
+      } else {
+        res.send({jobs: dbres});
+      }
+    });
+  };
 });
 
+
+/* GET details about a specific job */
 router.get('/:jobId', function(req, res, next) {
   jobId = req.params.jobId;
-  res.json({job_id: jobId, job_name: "myjob", job_status: "running", model_location: "here", data_location: "there", training_result_location: "no result yet"});
+  db = req.app.locals.dbClient.db("jobs");
+  db.collection("jobs").findOne({"_id": ObjectID(jobId)}, function(err, dbres) {
+    if (err) {
+      res.status(500);
+      res.send("Failed to retrieve the list of jobs!");
+    } else {
+      res.send(dbres);
+    }
+  });
+});
+
+
+/* POST submit a new job */
+router.post('/', function(req, res, next) {
+  dataConfig = req.body.data_config;
+  modelConfig = req.body.model_config;
+  modelName = req.body.model_name;
+  authorName = req.body.author_name;
+  db = req.app.locals.dbClient.db("jobs");
+
+  var jobObject = {
+    data_config: dataConfig,
+    model_config: modelConfig,
+    model_name: modelName,
+    author_name: authorName
+  };
+
+  console.log(jobObject);
+
+  db.collection("jobs").insertOne(jobObject, function(err, dbres) {
+    if (err) {
+      res.status(500);
+      res.send("Failed to submit the job!")
+    } else {
+      res.send({job_id: dbres.insertedId.toString()});
+    }
+  });
+});
+
+
+/* DELETE remove a job */
+router.delete('/:jobId', function(req, res, next) {
+  jobId = req.params.jobId;
+  db = req.app.locals.dbClient.db("jobs");
+  db.collection("jobs").deleteOne({"_id": ObjectID(jobId)}, function(err, dbres) {
+    if (err) {
+      res.status(500);
+      res.send("Failed to retrieve the list of jobs!");
+    } else {
+      res.send({status: "successful"});
+    }
+  });
 });
 
 module.exports = router;
