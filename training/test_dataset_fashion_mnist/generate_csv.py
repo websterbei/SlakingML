4054969e3b2749bed373822d1a0732576963b940
Webster Bei Yijie <yijie.bei@duke.edu>
2020-02-04 21:07:40 -0500

    reduced testdata size

--- training/test_dataset_fashion_mnist/generate_csv.py
+++ training/test_dataset_fashion_mnist/generate_csv.py
@@ -4,7 +4,7 @@ img = np.load("train_images.npy")
 label = np.load("train_labels.npy")
 
 with open("fashion_mnist.csv", "w") as f:
-    for i in range(55000):
+    for i in range(img.shape[0]):
         temp = [label[i]] + img[i].tolist()
         temps = [str(x) for x in temp]
         f.write(",".join(temps) + "\n")
