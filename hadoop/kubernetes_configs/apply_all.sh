9696cdee74be29afd6ff5171d19dcd5d4f80ddc3
Webster Bei Yijie <yijie.bei@duke.edu>
2020-02-19 19:55:51 -0500

    debug kuberntes hadoop nfs

---
+++ hadoop/kubernetes_configs/apply_all.sh
@@ -0,0 +1,1 @@
+kubectl apply -f namenode-deployment.yaml,namenode-service.yaml,namenode-hadoop-env-configmap.yaml,datanode-deployment.yaml,datanode-service.yaml,datanode-hadoop-env-configmap.yaml
---
+++ hadoop/kubernetes_configs/delete_all_resources.sh
@@ -0,0 +1,4 @@
+kubectl delete deployment namenode
+kubectl delete deployment datanode
+kubectl delete service namenode
+kubectl delete service datanode
