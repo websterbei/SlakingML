a0104e0b8041e65bfc6ee6d7375bf6129775b788
Webster Bei Yijie <yijie.bei@duke.edu>
2020-03-26 22:00:34 -0400

    work in progress, testing distributed training basic

--- training/training_src/dataset_factory.py
+++ training/training_src/dataset_factory.py
@@ -80,3 +80,77 @@ class CustomIterableDataset(IterableDataset):
                     #     yield np.array(features, dtype=np.float32)
                     yield np.array(line[self.feature_columns], dtype=np.float32), np.array([line[self.label_columns]], dtype=np.int64)
             f.close()
+
+class CustomDistributedIterableDataset(IterableDataset):
+    
+    def __init__(self, dataset_folder, label_columns=["label"], feature_columns=None, use_pyarrow=True, rank=0):
+        # read metadata
+        self.use_pyarrow = use_pyarrow
+        if use_pyarrow:
+            self.fs = pa.hdfs.connect('namenode', port=9000, driver='libhdfs3')
+        metadata = None
+        try:
+            if use_pyarrow:
+                with self.fs.open(os.path.join(dataset_folder, "metadata")) as f:
+                    metadata = json.load(f)
+            else:
+                with open(os.path.join(dataset_folder, "metadata")) as f:
+                    metadata = json.load(f)
+        except:
+            raise Exception("Did not find metadata file in given folder")
+        # basic error checking
+        if (metadata.get("column_names", None) is None) or (len(metadata.get("column_names")) == 0) \
+            or (metadata.get("column_types", None) is None) or (len(metadata.get("column_types")) == 0) \
+            or (len(metadata.get("column_names")) != len(metadata.get("column_types"))):
+            raise Exception("metadata incorrect")
+        
+        self.resource_files = [os.path.join(dataset_folder, resource_file) for resource_file in metadata["resource_files"]]
+
+        # if label_columns was set to None, no label is retrieved
+        if label_columns is None:
+            self.label_columns = None
+            self.label_types = None
+        else:
+            self.label_columns = [metadata["column_names"].index(label_name) for label_name in label_columns]
+            self.label_types = [metadata["column_types"][ind] for ind in self.label_columns]
+
+        # if feature_columns is set to None, then all columns except the label columns are retrieved
+        if feature_columns is None:
+            selected_features = list(metadata["column_names"])
+            if self.label_columns is not None:
+                for label_ind in self.label_columns:
+                    del selected_features[label_ind]
+        else:
+            selected_features = feature_columns
+        self.feature_columns = [metadata["column_names"].index(feature_name) for feature_name in selected_features]
+        self.feature_types = [metadata["column_types"][ind] for ind in self.feature_columns]
+
+    def _load_images(self, folder, filename):
+        pass
+
+    def _cast(self, string_value, dtype):
+        if dtype == "integer":
+            return int(string_value)
+        elif dtype == "float":
+            return float(string_value)
+        elif dtype == "image":
+            pass
+        else:
+            raise Exception("Unsupported datatype: " + dtype)
+
+    def __iter__(self):
+        for resource_file in self.resource_files:
+            if self.use_pyarrow:
+                f = self.fs.open(resource_file)
+            else:
+                f = open(resource_file)
+            for chunk in pd.read_csv(f, chunksize=2000):
+                for _, line in chunk.iterrows():
+                    # features = [self._cast(line[ind], dtype) for ind, dtype in zip(self.feature_columns, self.feature_types)]
+                    # if self.label_columns is not None:
+                    #     labels = [self._cast(line[ind], dtype) for ind, dtype in zip(self.label_columns, self.label_types)]
+                    #     yield np.array(features, dtype=np.float32), np.array(labels)
+                    # else:
+                    #     yield np.array(features, dtype=np.float32)
+                    yield np.array(line[self.feature_columns], dtype=np.float32), np.array([line[self.label_columns]], dtype=np.int64)
+            f.close()
--- training/training_src/stall.py
+++
@@ -1,8 +0,0 @@
-import time
-
-ind = 0
-while True:
-    if ind % 100 == 0:
-        print(ind)
-    time.sleep(1)
-    ind += 1
